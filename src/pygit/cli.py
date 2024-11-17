03f512d5293aa6f2a8c1161504d138346e95d601
Ismail <ismail@ismailtlemcani.com>
2024-11-17 11:21:49 +0100

    init an empty git

--- src/pygit/cli.py
+++ src/pygit/cli.py
@@ -2,63 +2,18 @@ import random
 import sys
 
 import click
+
 # from sorting_techniques import pysort
 
 
 @click.group()
 @click.version_option()
-def cli():
+def cli() -> None:
     "python git client"
 
 
-@cli.command(name="sort")
-@click.argument("file", type=click.File("r"))
-@click.option("-u", "--unique", is_flag=True, help="Get only unique words")
-@click.argument("head")
-@click.option(
-    "-n", "--number", default=1, help="Number of lines to get", type=click.INT
-)
-@click.option(
-    "--sorting-algorithm",
-    type=click.Choice(
-        ["quick-sort", "random-sort", "merge-sort"], case_sensitive=False
-    ),
-)
-def sort(file, unique, head, sorting_algorithm, number):
+@cli.command(name="init", help="init an empty git repository")
+def sort():
     "Command description goes here"
-
-    sort_obj = pysort.Sorting()
-
-    # for line in file.readlines():
-    lines = file.readlines()
-    print(sorting_algorithm)
-    # breakpoint()
-    if head:
-        sorted_list = (
-            sorted(list(set(lines)))[:number] if unique else sorted(lines)[:number]
-        )
-        if sorting_algorithm == "quick_sort":
-            sorted_list = (
-                sort_obj.heapSort(list(set(lines)))[:number]
-                if unique
-                else sort_obj.heapSort(lines)[:number]
-            )
-
-        elif sorting_algorithm == "merge_sort":
-            sorted_list = (
-                sort_obj.mergeSort(list(set(lines)))[:number]
-                if unique
-                else sort_obj.mergeSort(lines)[:number]
-            )
-
-        elif sorting_algorithm == "random_sort":
-            uniques_list_element = list(set(lines))
-            random_list = (
-                random.sample(uniques_list_element, len(uniques_list_element))[:number]
-                if unique
-                else random.sample(lines, len(lines))[:number]
-            )
-            return sys.stdout.write("".join(random_list))
-        return sys.stdout.write("".join(sorted_list))
-    sorted_list = sorted(lines)
-    return sys.stdout.write("".join(sorted_list))
+    print("hhellooooo init")
+    return "hello"
