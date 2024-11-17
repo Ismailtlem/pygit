b4cd7ca3e1939feda891664ea3a3daa8afa52fdb
Ismail <ismail@ismailtlemcani.com>
2024-11-17 17:02:54 +0100

    commit code

--- src/pygit/cli.py
+++ src/pygit/cli.py
@@ -1,19 +1,38 @@
-import random
-import sys
+import os
 
 import click
-
-# from sorting_techniques import pysort
 
 
 @click.group()
 @click.version_option()
 def cli() -> None:
-    "python git client"
+    "python git client"  # noqa: D300, D403
+    click.echo("Welcome to pygit!")
 
 
 @cli.command(name="init", help="init an empty git repository")
-def sort():
-    "Command description goes here"
-    print("hhellooooo init")
+def init():
+    """Command description goes here."""
+    DIRECTORY_NAME = ".git"
+    DIRS_LIST = ["hooks", "info", "objects", "refs"]
+    try:
+        os.mkdir(f"{DIRECTORY_NAME}")
+        print(f"Directory '{DIRECTORY_NAME}' created successfully.")
+    except FileExistsError:
+        print(f"Directory '{DIRECTORY_NAME}' already exists.")
+    except PermissionError:
+        print(f"Permission denied: Unable to create '{DIRECTORY_NAME}'.")
+    # for file in FILES_LIST:
+    with open(f"{DIRECTORY_NAME}/HEAD", "w") as f:
+        f.write("ref: refs/heads/master")
+    with open(f"{DIRECTORY_NAME}/config", "w") as f:
+        f.write("")
+    with open(f"{DIRECTORY_NAME}/description", "w") as f:
+        f.write(
+            "Unnamed repository; edit this file 'description' to name the repository."
+        )
+
+    for folder in DIRS_LIST:
+        os.mkdir(f"{DIRECTORY_NAME}/{folder}")
+
     return "hello"
